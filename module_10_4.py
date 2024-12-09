78000d0b0d6678522503a5d8db1599d5cf1d4eef
Stella0910 <aniretake0910@gmail.com>
2024-12-09 20:00:40 +0300

    Add files via upload

--- module_10_4.py
+++ module_10_4.py
@@ -21,7 +21,6 @@ class Guest(threading.Thread):
         time.sleep(random.randint(3, 10))
 
 
-
 class Cafe:
 
     def __init__(self, *tables):
@@ -30,31 +29,33 @@ class Cafe:
 
     def guest_arrival(self, *guests):
         self.guests = [*guests]
-        waitimg = [] # временный список ожидания (для проверки)
         for guest in self.guests:
             for table in self.tables:
+                if not table.guest is None:
+                    if all(table.guest is not None for table in self.tables):
+                        print(f'{guest.name} в очереди')
+                        self.queue.put(guest)
+                        break
                 if table.guest is None:
                     table.guest = guest.name
-                    # self.guests.pop(0)
+                    Guest.start(guest)
+                    Guest.join(guest)
                     print(f'{guest.name} сел(-а) за стол номер {table.number}')
                     break
-                else:
-                    continue
 
-
-        print(self.guests)
-        #     waitimg.append(guest.name)
-        #     print(f'{guest.name} в очереди')
-        # print(waitimg)
-
-
-
-    #     self.queue.put(guest.name)
-    #     guest_ = Guest(self.guests)
-    #     guest_.start()
-    #     guest_.join()
-    #     print(f'{guest.name} сел(-а) за стол номер {table.number}')
-
+    def discuss_guests(self):
+        while not self.queue.empty():
+            for table in self.tables:
+                if not Guest(table.guest).is_alive():
+                    print(f'{table.guest} за текущим столом покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {table.number} свободен')
+                    table.guest = None
+                    if not self.queue.empty():
+                        guest = self.queue.get()
+                        table.guest = guest.name
+                        print(f'{table.guest} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                        Guest.start(guest)
+                        Guest.join(guest)
 
 
 tables = [Table(number) for number in range(1, 6)]
@@ -63,3 +64,4 @@ guests_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
 guests = [Guest(name) for name in guests_names]
 cafe = Cafe(*tables)
 cafe.guest_arrival(*guests)
+cafe.discuss_guests()
